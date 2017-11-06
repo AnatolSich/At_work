c06ad50511588fb70597f5240a2c28571bcf1c17
AnatolSich <tip-77@ukr.net>
2017-11-06 17:26:37 +0200

    arrayReplace(2 methods)

--- arrayReplace.java
+++ arrayReplace.java
@@ -10,15 +10,20 @@ import java.util.List;
 public class arrayReplace {
 
     public static void main(String[] args) {
-        int[] arrayInput = new int[]{21, 20, 50, 21, 20, 50, 21, 20, 50, 21, 20};
-        int[] arrayFind = new int[]{21, 20, 50, 21, 20};
-        int[] arrayReplace = new int[]{70, 465, 23, 23};
-        int[] arrayReplace2 = sweep(arrayInput, 4,5);
+        int[] arrayInput = new int[]{1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4};
+        int[] arrayFind = new int[]{1,2};
+        int[] arrayReplace = new int[]{8, 8, 8};
+        int[] arrayReplace2 =
+
+                sweep(arrayInput, 4, arrayReplace, arrayFind);
+
+
+        //       finding(arrayInput, arrayFind, arrayReplace);
+
 
         for (int i = 0; i < arrayReplace2.length; i++) {
-            System.out.print(arrayReplace2[i]+" ");
+            System.out.print(arrayReplace2[i] + " ");
         }
-
 
 
     }
@@ -31,23 +36,31 @@ public class arrayReplace {
 
     }
 */
-    private static int[] sweep(int[] arrayIn, int index, int length) {
-        int[] out = new int[arrayIn.length + length];
-        int j=0;
+    private static int[] sweep(int[] arrayIn, int index, int[] arrayReplace, int[] arrayFind) {
+        int[] out = new int[arrayIn.length + arrayReplace.length - arrayFind.length];
+        int j = 0;
+        int k = 0;
         for (int i = 0; i < out.length; i++) {
-            if (i >= index & i < index + length)
-                out[i] = 0;
-            else {
-                out[i]=arrayIn[j++];
+            if (i >= index & i < index + arrayReplace.length) {
+
+                out[i] = arrayReplace[k++];
+
+            } else {
+                if (k != 0) out[i] = arrayIn[k - 1 + j++];
+                else out[i] = arrayIn[k + j++];
             }
         }
         return out;
     }
 
 
-    private static List<Integer> finding(int[] arrayInput, int[] arrayFind) {
+    private static int[] finding(int[] arrayInput, int[] arrayFind, int[] arrayReplace) {
 
-        int[] out = new int[arrayInput.length/arrayFind.length+1];
+        int[] out = new int[arrayInput.length];
+        System.arraycopy(arrayInput, 0, out, 0, arrayInput.length);
+
+
+        int insertCount = 0;
         int count = 0;
 
         for (int i = 0; i <= arrayInput.length - (arrayFind.length - count); ) {
@@ -57,8 +70,10 @@ public class arrayReplace {
                 count++;
 
                 if (count == arrayFind.length) {
-                   // out[](i - (count - 1));
-                    System.out.println(i - (count - 1));
+                    // out[](i - (count - 1));
+                    //                  System.out.println(i - (count - 1) + insertCount * (arrayReplace.length - arrayFind.length));
+                    out = sweep(out, i - (count - 1) + insertCount * (arrayReplace.length - arrayFind.length), arrayReplace, arrayFind);
+                    insertCount++;
                     // i = i - (count - 1) + 1;
                     count = 0;
                     continue;
@@ -77,7 +92,7 @@ public class arrayReplace {
 
         }
 
-
+        return out;
     }
 
 }
