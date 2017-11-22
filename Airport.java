300e864c38d7aa898d17386c58d5ac5d8fc2c352
AnatolSich <tip-77@ukr.net>
2017-11-22 10:56:24 +0200

    Made StreamPlanes and CountDown

---
+++ Airport.java
@@ -0,0 +1,106 @@
+/**
+ * Created by Toll on 22.11.2017.
+ */
+
+/* Аэропорт
+1. В выполняющем классе создать метод waiting(), который отправляет нить в сон на 100мс
+2. В выполняющем классе создать метод takingOff(), который отправляет нить в сон на 100мс
+3. Создать класс Runway (взлетная полоса)
+3.1 В классе Runway:
+- создать приватную переменную типа Thread
+- для переменной создать геттер и сеттер
+4. В выполняющем классе создать статическую переменную RUNWAY, подумать, какой тип у данной переменной
+5. Создать класс Plane, унаследовать его от Thread
+6. В классе Plane:
+- создать конструктор с параметром name, передать переменную name в конструктор суперкласса
+- в конструкторе запустить нить
+- реализовать метод run():
+6.1 создать переменную isAlreadyTakenOff, подумать какой тип у переменной, инициализировать её значением по-умолчанию
+6.2 пока переменная isAlreadyTakenOff имеет значение false, выполнять следующее:
+- ЕСЛИ взлетная полоса свободна, занимать её
+- выводить на экран надпись getName() + " взлетает"
+- производить взлет самолета
+- выводить на экран надпись getName() + " уже в небе"
+- присвоить переменной isAlreadyTakenOff значение true
+- освободить взлетную полосу
+- ИНАЧЕ, если взлетная полоса занята самолетом
+- выводить на экран надпись getName() + " ожидает"
+- вызвать метод, который отвечает за ожидание
+7. В выполняющем методе создать и запустить 3 нити(самолета)
+*/
+
+public class Airport {
+    static Run_way RUNWAY = new Run_way();
+
+    public static void main(String[] args) {
+
+        new PPlane("Ласточка");
+        new PPlane("Руслан");
+        new PPlane("Мрия");
+    }
+
+
+
+    static void waiting() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void takingOff() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+class Run_way {
+    boolean checking = false;
+    Thread plane=null;
+
+    public Thread getPlane() {
+        return plane;
+    }
+
+    public void setPlane(Thread plane) {
+        this.plane = plane;
+    }
+
+
+}
+
+class PPlane extends Thread {
+    boolean isAlreadyTakenOff =false;
+
+
+    public PPlane(String name) {
+        super(name);
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        while (!isAlreadyTakenOff) {
+            if (Airport.RUNWAY.getPlane()==null) {
+
+                Airport.RUNWAY.setPlane(this);
+                System.out.println(getName() + " взлетает");
+                Airport.takingOff();
+                System.out.println(getName() + " уже в небе");
+                this.isAlreadyTakenOff = true;
+                Airport.RUNWAY.setPlane(null);
+
+
+            } else {
+                System.out.println(getName() + " ожидает");
+                Airport.waiting();
+
+            }
+        }
+    }
+}
--- StreamPlanes.java
+++ StreamPlanes.java
@@ -1,4 +1,6 @@
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -10,27 +12,30 @@ import java.util.List;
 2. Самолет занимает свободную полосу и взлетает (взлет длится 2 сек)
 3. После взлета вышка проводит самопроверку состояния (1 сек) и после этого готова к новому взлету
 
-4. Башня контролирует взлеты (начинает/заканчивает взлеты, ищет свободную полосу и тд)
+4. Башня контролирует взлеты (начинает взлеты, ищет свободную полосу и тд)
 
 */
 public class StreamPlanes {
     static int rithm = 500;
-    static int planeOff = 3*rithm;
-    static int runwayCheck = 3*rithm;
-    static int planeWait=2*rithm;
-
-
+    static int planeOff = 4 * rithm;
+    static int runwayCheck = 3 * rithm;
+    static int planeWait = 3 * rithm;
+    //static SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss:SSS");
+    static SimpleDateFormat dateFormat = new SimpleDateFormat("ss:SSS");
 
     public static void main(String[] args) {
+
+
+
         List<Plane> planes = new ArrayList<>();
         List<Runway> runways = new ArrayList<>();
         FlightTower flightTower = new FlightTower(planes, runways);
-        Plane An225 = new Plane("An-225",flightTower);
-        Plane An148 = new Plane("An-148",flightTower);
-        Plane An158 = new Plane("An-158",flightTower);
-        Plane An178 = new Plane("An-178",flightTower);
-        Plane An124 = new Plane("An-124",flightTower);
-        Plane An70 = new Plane("An-70",flightTower);
+        Plane An225 = new Plane("An-225", flightTower);
+        Plane An148 = new Plane("An-148", flightTower);
+        Plane An158 = new Plane("An-158", flightTower);
+        Plane An178 = new Plane("An-178", flightTower);
+        Plane An124 = new Plane("An-124", flightTower);
+        Plane An70 = new Plane("An-70", flightTower);
 
         planes.add(An225);
         planes.add(An148);
@@ -39,18 +44,28 @@ public class StreamPlanes {
         planes.add(An124);
         planes.add(An70);
 
+        Runway runway001 = new Runway("Runway001", flightTower);
+        Runway runway002 = new Runway("Runway002", flightTower);
+        Runway runway003 = new Runway("Runway003", flightTower);
 
-        runways.add(new Runway("Runway001",flightTower));
-        runways.add(new Runway("Runway002",flightTower));
+
+
+        runways.add(runway001);
+        runways.add(runway002);
+        runways.add(runway003);
 
 
 
+        Date start = new Date();
         flightTower.startFlying();
 
+        while (!flightTower.isEveryPlaneAway()) {
 
-        if(flightTower.isEveryPlaneAway()) {
-            System.out.println("Полеты закончены");
         }
+        Date finish = new Date();
+
+        System.out.println("Полеты длились  " + (finish.getTime() - start.getTime()) + "ms");
+
 
     }
 
@@ -68,7 +83,7 @@ class FlightTower {
     }
 
     public void waiting(Thread thread) {
-        System.out.println(thread.getName() + " waiting...");
+        System.out.println(StreamPlanes.dateFormat.format(new Date().getTime())+" "+thread.getName() + " waiting...");
         try {
             Thread.sleep(StreamPlanes.planeWait);
         } catch (InterruptedException e) {
@@ -77,14 +92,14 @@ class FlightTower {
 
     }
 
-    public Runway freeRunway() {
+    public synchronized Runway freeRunway() {
         for (Runway runway : runways
                 ) {
-            if (runway.isReady) {
+            if (runway.isReady == true && runway.plane == null) {
                 return runway;
             }
         }
-        Runway nullRunway = new Runway("NULL",this);
+        Runway nullRunway = new Runway("NULL", this);
         return nullRunway;
     }
 
@@ -122,9 +137,9 @@ class Plane extends Thread {
     boolean isAlreadyTakenOff;
     FlightTower flightTower;
 
-    public Plane(String name,FlightTower flightTower) {
+    public Plane(String name, FlightTower flightTower) {
         super(name);
-        this.flightTower=flightTower;
+        this.flightTower = flightTower;
 
     }
 
@@ -132,18 +147,16 @@ class Plane extends Thread {
     public void run() {
 
 
-
         while (!isAlreadyTakenOff) {
 
             Runway runway = flightTower.freeRunway();
-            if(!runway.getName().equals("NULL")) {
+            if (!runway.getName().equals("NULL")) {
                 runway.plane = this;
                 runway.isReady = false;
-                System.out.println(this.getName() + " взлетает на полосе "+ runway.getName());
+                System.out.println(StreamPlanes.dateFormat.format(new Date().getTime()) + " " + this.getName() + " взлетает на полосе " + runway.getName());
                 takingAway();
                 runway.plane = null;
-            }
-            else
+            } else
                 flightTower.waiting(this);
 
         }
@@ -157,7 +170,7 @@ class Plane extends Thread {
             e.printStackTrace();
         }
         this.isAlreadyTakenOff = true;
-        System.out.println(this.getName() + " is away");
+        System.out.println(StreamPlanes.dateFormat.format(new Date().getTime()) + " " + this.getName() + " is away");
     }
 
 }
@@ -167,9 +180,9 @@ class Runway extends Thread {
     Plane plane;
     FlightTower flightTower;
 
-    public Runway(String name,FlightTower flightTower) {
+    public Runway(String name, FlightTower flightTower) {
         super(name);
-        this.flightTower=flightTower;
+        this.flightTower = flightTower;
     }
 
     @Override
@@ -177,18 +190,17 @@ class Runway extends Thread {
         this.selfchecking();
         while (!flightTower.isEveryPlaneAway()) {
             if (this.plane == null && this.isReady == false) {
+                System.out.println(StreamPlanes.dateFormat.format(new Date().getTime()) + " " + this.getName() + " занята...");
                 this.selfchecking();
-            }
-            else if (this.plane != null && this.isReady == false){
-                System.out.println(this.getName()+ " занята...");
+            } else if (this.plane != null && this.isReady == false) {
+                System.out.println(StreamPlanes.dateFormat.format(new Date().getTime()) + " " + this.getName() + " занята...");
                 try {
                     Thread.sleep(StreamPlanes.rithm);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            }
-            else if (this.plane == null && this.isReady == true) {
-                System.out.println(this.getName()+ " свободна...");
+            } else if (this.plane == null && this.isReady == true) {
+                System.out.println(StreamPlanes.dateFormat.format(new Date().getTime()) + " " + this.getName() + " свободна...");
                 try {
                     Thread.sleep(StreamPlanes.rithm);
                 } catch (InterruptedException e) {
